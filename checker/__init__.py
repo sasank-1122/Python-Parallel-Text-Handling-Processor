ff98601aa04b4dbe27b2d80b6a470d66084f711b
sasank <sasankkodapaka95@gmail.com>
2025-11-25 16:53:36 +0530

    Updated Files

--- checker/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Top-level checker package shim that delegates to `app.checker`.
-This allows imports like `from checker.rules import ...` to work.
-"""
-import os
-
-_app_checker = os.path.normpath(os.path.join(os.path.dirname(__file__), '..', 'app', 'checker'))
-if _app_checker not in __path__:
-    __path__.insert(0, _app_checker)
